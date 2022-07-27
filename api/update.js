47f32788309f71273fa4ab00d02ba8ded3fa3d55
gyhyfj <gyhyfj@gmail.com>
2022-07-27 23:07:52 +0800

    try fixing bug

--- api/update.js
+++ api/update.js
@@ -2,7 +2,13 @@ const axios = require('axios')
 
 module.exports = async (req, res) => {
   let idk = await axios.get(
-    'https://yachen.cc:9001/getVidInfo?bvidArr=' + req.query.bvidArr
+    // 'https://yachen.cc:9001/getVidInfo?bvidArr=' + req.query.bvidArr
+    'https://yachen.cc:9001/getVidInfo',
+    {
+      params: {
+        bvidArr: req.query.bvidArr,
+      },
+    }
   )
   res.send(idk.data)
 }
